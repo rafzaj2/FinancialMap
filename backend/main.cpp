bc75ceaa9c2fa0f8e1742b10bb691ed1254c77e3
r.zajac <r.zajac@partner.samsung.com>
2019-06-21 20:36:03 +0200

    Change the way of handling login procedure

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,74 +1,41 @@
 #include <pistache/endpoint.h>
 #include <iostream>
-#include <fstream>
-#include <string>
+#include <nlohmann/json.hpp>
 
 using namespace Pistache;
+using json = nlohmann::json; 
 
 struct HelloHandler : public Http::Handler {
   int counter;
   HTTP_PROTOTYPE(HelloHandler)
   void onRequest(const Http::Request& request, Http::ResponseWriter writer) override{
-    printinfo();
+
     std::cout << "Pistache got request: " << request.method() << std::endl;
     std::cout << "Method = " << request.method() << std::endl;
     std::cout << "Body = " << request.body() << std::endl;
+    auto myHeader = request.headers().getRaw("x-raw-header");
+    std::cout << "Header name is " << myHeader.name() << std::endl;
+    std::cout << "Header value is " << myHeader.value() << std::endl;
 
-    //**********************************************************
-    // std::fstream finmapHTMLPage;
-    // finmapHTMLPage.open("../frontend/src/finmap.html", std::ios::in);
-    // if (!finmapHTMLPage.good())
-    // {
-    //   std::cout << "File haven't been opened succeesfully" << std::endl;
-    // }
+    json userData = json::parse(request.body());
 
-    
-    // char buffer2[ 1024 ];
-    // std::streamsize fsize = finmapHTMLPage.;
-    // //finmapHTMLPage.seekg(0, ios::beg);
-    // finmapHTMLPage.read(buffer2, 1024);
-    // finmapHTMLPage.close();
-    // if (buffer2 == NULL)
-    // {
-    //     std::cout << "buffer is null" << std::endl;
-    // }
-    // else
-    // {
-    //   //std::cout << "buffer" << buffer[0] << buffer[1] << std::endl;
-    //   std::cout << "Wczytano " << finmapHTMLPage.gcount() << " bajtów do buffer2" << std::endl;
-    // }
 
-    //**********************************************************
+    std::cout << "Json userData = " << userData << std::endl;
 
-    std::fstream finmapHTMLPage;
-    std::string buffer;
-    std::string line;
-    finmapHTMLPage.open("../frontend/src/finmap.html", std::ios::in);
-    if (finmapHTMLPage.good())
-    {
-      while (!finmapHTMLPage.eof())
-      {
-        getline(finmapHTMLPage, line);
-        buffer.append(line);
-      }
-      finmapHTMLPage.close();
-      std::cout << "File haven been read succeesfully" << std::endl;
-    }
-    else
-    {
-      std::cout << "File haven't been opened succeesfully" << std::endl;
-    }
-    
-    writer.send(Http::Code::Ok, buffer);
-    std::cout << "Server've reponded succeesfully and sent a " << buffer.size() <<std::endl;
-  }
-  void printinfo()
-  {
-    counter++;
-    std::cout << "Pistache server is going to send \"Hello World!\" message for the " << counter << " time."<< std::endl;
+    writer.send(Http::Code::Ok, "<html>Hello, World!</html>");
   }
 };
 
 int main() {
   Http::listenAndServe<HelloHandler>("*:9080");
+}
+
+
+int loginProcedure(json)
+{
+  //Todo
+}
+int registerProcedure(json)
+{
+  //Todo
 }
